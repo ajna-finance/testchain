07473ce84c1815c921bbfc8e98094d43fb36dd9b
Ed Noepel <ed@noepel.net>
2023-06-26 12:00:27 -0400

    do not change command when publishing

--- publish.sh
+++ publish.sh
@@ -7,5 +7,5 @@ echo "Use your GitHub username and paste your GitHub token as the password."
 docker login ghcr.io || exit 2
 
 # push the package to the GitHub Container Repository
-docker commit --change='CMD ["--db", "/app/data", "--accounts", "16", "--wallet.seed", "20070213", "--port", "8555", "--fork", "${MAINNET_FORK_URL}", "-v"]' ajna-testnet ghcr.io/ajna-finance/ajna-testnet:${tag} || exit 3
+docker commit ajna-testnet ghcr.io/ajna-finance/ajna-testnet:${tag} || exit 3
 docker push ghcr.io/ajna-finance/ajna-testnet:${tag}
